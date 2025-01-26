1c9596b5cc1536b9e94f20c6f2b67cd313975962
Fernando Aguilar <feraguilar6985@gmail.com>
2025-01-26 17:41:57 -0600

    Add CORS middleware to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests #type: ignore
 
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT", "PATCH", "DELETE"],
+    allow_headers=["Authorization", "Content-Type", "X-API-Key"],
+)
 
 @app.get("/")
 def read_root():
